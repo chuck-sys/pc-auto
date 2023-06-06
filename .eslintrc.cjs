dfb2c591449629356d7c3790bd83eb7ed89938a2
Cheuk Yin Ng <chucksys88@gmail.com>
2023-06-05 20:48:45 -0700

    ci: add format/linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
@@ -9,7 +9,10 @@ module.exports = {
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    semi: [2, 'always'],
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
-}
+};
